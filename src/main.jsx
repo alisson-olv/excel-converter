d8420162ec5e0138a04d3e2bfc54b1b5786991ec
Alisson Oliveira <binho_alisson@hotmail.com>
2023-11-01 00:41:03 -0300

    update: name of folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
-import ModalContext from './context/ModalContext'
+import ModalContext from './context/ModalContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
